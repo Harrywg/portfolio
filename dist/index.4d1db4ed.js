1d02f665ffe4405e819a6dab3cdfbea5e75a5a42
Harry Ward-Gray <wardgray24@gmail.com>
2023-09-07 22:50:14 +0100

    2.0.8 fix buttons.js not imported

--- dist/index.4d1db4ed.js
+++ dist/index.4d1db4ed.js
@@ -4860,54 +4860,14 @@ var CSSPlugin = {
 },{"./gsap-core.js":"05eeC","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"4cqvf":[function(require,module,exports) {
 // DISCLAIMER!!!!
 // - If you're looking through my portfolio to check out my code standard, please check out my most recent projects as this code is now very old and messy.
-function scrollToSection(elTag) {
-    let section = document.querySelector(elTag);
+function scrollToSection(e) {
+    let section = document.getElementById(e.target.name);
     let rect = section.getBoundingClientRect();
     document.getElementById("main").scrollTo(rect.x, rect.y);
 }
-function projectInfo(id) {
-    document.querySelector(".project_info-button").style.display = "block";
-    let project = document.getElementById(id);
-    project.style.zIndex = 10;
-    project.style.pointerEvents = "none";
-    project.children[0].classList.add("project_image-active");
-    document.querySelector(":root").style.setProperty("--project-img-overlay", "transparent");
-    project.lastElementChild.classList.add("project_info-wrap-active");
-    let titles = Array.from(document.getElementsByClassName("project_title"));
-    titles.forEach((title)=>{
-        title.style.zIndex = 1;
-    });
-    project.children[1].style.display = "none";
-    project.style.pointerEvents = "all !important";
-}
-function projectInfoBack(id) {
-    document.querySelector(".project_info-button").style.display = "none";
-    let project = document.querySelector(".project_image-active").parentElement;
-    project.children[2].style.transition = "clip-path 0s";
-    project.style.pointerEvents = "all";
-    project.style.zIndex = 0;
-    project.lastElementChild.classList.remove("project_info-wrap-active");
-    project.children[0].classList.remove("project_image-active");
-    let titles = Array.from(document.getElementsByClassName("project_title"));
-    titles.forEach((title)=>{
-        title.style.zIndex = 10;
-    });
-    project.children[1].style.display = "grid";
-    document.querySelector(":root").style.setProperty("--project-img-overlay", " #0f695488");
-    setTimeout(()=>{
-        project.children[2].style.transition = "clip-path 2s";
-    });
-}
+const sectionLinks = document.getElementsByClassName("section-link");
+Array.from(sectionLinks).forEach((link)=>link.onclick = scrollToSection);
 function handleFormSubmit(button) {
-    // $.ajax({
-    //     url: "https://formsubmit.co/ajax/your@email.com",
-    //     method: "POST",
-    //     data: {
-    //         name: "FormSubmit",
-    //         message: "I'm from Devro LABS"
-    //     },
-    //     dataType: "json"
-    // });
     let form = button.parentElement;
     if (!form.elements[0].value || !form.elements[1].value || !form.elements[2].value || !form.elements[3].value) return;
     form.style.pointerEvents = "none";
